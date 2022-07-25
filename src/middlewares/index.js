3f3a22a1042d1d174037871f89e228a4b034594d
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-07-25 16:30:20 -0300

    exporta novo middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,9 @@
 const validateEmail = require('./validateEmail');
 const validatePassword = require('./validatePassword');
+const validateDisplayName = require('./validateDisplayName');
 
 module.exports = {
   validateEmail,
   validatePassword,
+  validateDisplayName,
 };
