53e42c5e9ee972bbe8f38ff0613e1b8c487ec068
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-07-25 16:03:29 -0300

    Cria user model

---
+++ src/database/models/user.js
@@ -0,0 +1,21 @@
+const User = (sequelize, DataTypes) => {
+  const tableUser = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    timestamps: false,
+  });
+
+  return tableUser;
+};
+
+module.exports = User;
